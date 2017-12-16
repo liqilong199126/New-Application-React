6502c4e7df6f9c0351393b6ebecadfc16ae5518d
liqilong <634505724@qq.com>
2017-12-16 12:13:43 +0800

    注册功能和登录功能组件的开发

---
+++ src/js/components/mobile_index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MobileHeader from './mobile_header.js';
+import MobileFooter from './mobile_footer.js';
+
+export default class MobileIndex extends React.Component {
+	render() {
+		return(
+           <div>
+              <MobileHeader></MobileHeader>
+              <MobileFooter></MobileFooter>
+           </div>
+		);
+	};
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
+import MediaQuery from 'react-responsive';
 
 import PCIndex from './components/pc_index.js';
-
+import MobileIndex from './components/mobile_index.js';
 
 import { Button } from 'antd';
 
@@ -13,7 +14,12 @@ export default class Index extends React.Component{
 	render() {
 		return (
             <div>
-               <PCIndex/>
+               <MediaQuery query='(min-device-width: 1224px)'>
+                   <PCIndex/>
+               </MediaQuery>
+               <MediaQuery query='(max-device-width: 1224px)'>
+                   <MobileIndex/>
+               </MediaQuery>
             </div>
 		);
 	};
